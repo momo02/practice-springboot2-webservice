592e4e4da62469cc715365e7b5d87ffcae4bf32d
momo02 <jungha0902@gmail.com>
2021-07-18 15:45:52 +0900

    - gradle 버전 다운그레이드 - 롬복 적용 Dto 생성 및 테스트 코드 작성

--- src/main/java/org/example/book/springboot/web/HelloController.java
+++ src/main/java/org/example/book/springboot/web/HelloController.java
@@ -1,10 +1,17 @@
 package org.example.book.springboot.web;
 
+import org.example.book.springboot.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount){
+        return new HelloResponseDto(name,amount);
+    }
 
     @GetMapping("/hello")
     public String hello(){
