f228c3e02933c81203db451e0a520da240f30512
momo02 <jungha0902@gmail.com>
2021-08-01 17:42:00 +0900

    머스테치 템플릿 엔진 적용. 기본 페이지 만들기

---
+++ src/main/java/org/example/book/springboot/web/IndexController.java
@@ -0,0 +1,16 @@
+package org.example.book.springboot.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index(){
+        //머스테치 스타터 덕분에 컨트롤러에 문자열을 반환할 때, 앞의 경로(prefix)와 뒤의 파일 확장자(suffix)는 자동으로 지정.
+        //default prefix : src/main/resources/templates
+        //default suffix : .mustache
+        return "index";
+    }
+}
